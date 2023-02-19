c16eece13f603ca6f3f029f9e7b971f3f3baa3a8
Alexey Gavrilov <agavrilov@telenordigital.com>
2023-02-19 16:29:18 +0100

    Update runners

--- run.sh
+++ run.sh
@@ -1,3 +1,1 @@
-java -cp application/target/application-1.0-SNAPSHOT-dir/\* org.example.WordReverser
-java -cp application/target/application-1.0-SNAPSHOT-dir/\* org.example.JsonParser
-java -cp application/target/application-1.0-SNAPSHOT-dir/\* org.example.ResourceLoader
+java -cp application/target/application-1.0-SNAPSHOT-dir/\* org.example.Main
